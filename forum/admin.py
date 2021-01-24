396c9656352245f74840f2054aaa61171b042e48
Matthew Ingwersen <matttpt@gmail.com>
2021-01-24 18:32:40 -0500

    Remove custom form widget configuration
    These are not necessary (since I changed the Post and Comment content
    field types from CharField to TextField before the initial commit).

--- forum/admin.py
+++ forum/admin.py
@@ -1,16 +1,7 @@
 from django.contrib import admin
 
-from .forms import CommentAdminForm, PostAdminForm
 from .models import Comment, Post
 
 
-class PostAdmin(admin.ModelAdmin):
-    form = PostAdminForm
-
-
-class CommentAdmin(admin.ModelAdmin):
-    form = CommentAdminForm
-
-
-admin.site.register(Post, PostAdmin)
-admin.site.register(Comment, CommentAdmin)
+admin.site.register(Post)
+admin.site.register(Comment)
--- forum/forms.py
+++ forum/forms.py
@@ -1,28 +1,12 @@
 from django import forms
-from django.contrib import admin
 
 from .models import Comment, Post
-
-
-class PostAdminForm(forms.ModelForm):
-    class Meta:
-        model = Post
-        widgets = {"content": admin.widgets.AdminTextareaWidget()}
-        fields = "__all__"
-
-
-class CommentAdminForm(forms.ModelForm):
-    class Meta:
-        model = Comment
-        widgets = {"content": admin.widgets.AdminTextareaWidget()}
-        fields = "__all__"
 
 
 class EditPostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ["title", "content"]
-        widgets = {"content": forms.Textarea()}
         labels = {"content": "Your post"}
 
 
@@ -30,5 +14,4 @@ class EditCommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ["content"]
-        widgets = {"content": forms.Textarea()}
         labels = {"content": "Your comment"}
